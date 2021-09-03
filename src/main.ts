6798edce5c59d11cc3e3a5afffde638a4db66ca8
Kajiwara Kensei <kajiwara.k@icraft.jp>
2021-09-03 15:32:23 +0900

    TEST

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-console.log("1");
+import { searchMap } from "~/GoogleMap";
+import { readUserInput } from "~/util/index";
+
+readUserInput("GoogleMap検索 > ").then(searchMap);
+//searchMap("おでん");
---
+++ src/util/index.ts
@@ -0,0 +1,37 @@
+import Readline from "readline";
+/**
+ * 指定秒待ちます
+ * @module delay
+ * @param {number} second - 待ちたい秒数
+ */
+export function delay(second: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, second * 1000);
+  });
+}
+
+/**
+ * キーボードからの入力
+ * @module delay
+ * @param {string} question コンソールに表示される文字
+ * @returns {Promise<string>} 入力された文字
+ */
+export function readUserInput(question: string): Promise<string> {
+  const readline = Readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise<string>((resolve) => {
+    readline.question(question, (answer: string) => {
+      resolve(answer);
+      readline.close();
+    });
+  });
+}
+
+export const loop = (count: number) => {
+  return (callback: (value: number, index: number) => void): void => {
+    [...Array<number>(count)].map(callback);
+  };
+};

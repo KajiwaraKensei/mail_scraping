7423d5a10b02072f978b7b0014dea15e78702b1e
Kajiwara Kensei <kajiwara.k@icraft.jp>
2021-09-04 21:29:03 +0900

    nextjs init

---
+++ nextjs/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import React from "react";
+import styles from "../styles/Home.module.css";
+import styled from "styled-components";
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Title>Hello World!!</Title>
+    </div>
+  );
+};
+
+const Title = styled.h1``;
+
+export default Home;

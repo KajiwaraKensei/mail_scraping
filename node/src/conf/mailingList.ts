45a6275826c43179590a5127cd86c7d446e47e63
Kajiwara Kensei <kajiwara.k@icraft.jp>
2021-09-13 11:26:47 +0900

    csv の階層を変更

--- node/src/conf/mailingList.ts
+++ node/src/conf/mailingList.ts
@@ -1,3 +1,3 @@
-export const MAIL_CSV = "csv/a_mail.csv";
-export const MAIL_U_CSV = "csv/u_mail.csv";
-export const MAILING_LIST_CSV = "csv/mailing_list.csv";
+export const MAIL_CSV = "../csv/a_mail.csv";
+export const MAIL_U_CSV = "../csv/u_mail.csv";
+export const MAILING_LIST_CSV = "../csv/mailing_list.csv";
